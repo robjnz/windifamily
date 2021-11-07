b0d893733fd08e3e28fea81ce0572e9e4dd2d8aa
robin <robjnz@outlook.com>
2021-11-07 16:30:33 +0000

    add facebook link

--- src/components/TheFooter/TheFooter.vue
+++ src/components/TheFooter/TheFooter.vue
@@ -33,5 +33,14 @@
         src="https://res.cloudinary.com/dzhbfdfa5/image/upload/v1608989313/youtube_k4uh6z.jpg"
         alt="youtube link"
     /></g-link>
+
+    <g-link
+      class="ml-10 w-6 display:inline-grid"
+      href="https://bit.ly/3qdBL54"
+      target="_blank"
+      ><g-image
+        src="https://res.cloudinary.com/dzhbfdfa5/image/upload/c_scale,h_50,w_50/v1636300864/facebook_logo_nhdipi.jpg"
+        alt="youtube link"
+    /></g-link>
   </div>
 </template>
